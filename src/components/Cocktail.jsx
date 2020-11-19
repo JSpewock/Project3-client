d24795f8ce0d3c0cbca4d4287f595ce76e8e7452
Jeremy <jeremy.spewock@gmail.com>
2020-11-19 15:26:17 -0500

    added a loop to loop through ingredients and render them (JS/BT/RL)

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -2,21 +2,46 @@ import React, { Component } from 'react'
 
 export default class Cocktail extends Component {
     render() {
+        const ingredients = []
+        for (let i = 1; i < 16; i++) {
+            let numberString = i.toString()
+            ingredients.push('strIngredient' + numberString)
+        }
+        // console.log(ingredients)
         return (
             <div id={this.props.cocktail.strDrink} key={this.props.cocktail._id}>
                 <h1>{this.props.cocktail.strDrink}</h1>
                 <img src={this.props.cocktail.strDrinkThumb} />
                 <p>{this.props.cocktail.strInstructions}</p>
                 <p>{this.props.cocktail.strAlcoholic}</p>
-                <p>{this.props.cocktail.stsIngredient1}</p>
-                <p>{this.props.cocktail.}</p>
-                <p>{this.props.cocktail.}</p>
-                <p>{this.props.cocktail.}</p>
-                <p>{this.props.cocktail.}</p>
-                <p>{this.props.cocktail.}</p>
-                <p>{this.props.cocktail.}</p>
-                <p>{this.props.cocktail.}</p>
+
+    
+                {ingredients.map(ingredient => {
+                    
+                    console.log(this.props.cocktail[ingredient])
+
+
+                    if (this.props.cocktail[ingredient] !== null) {
+                        return <p>{this.props.cocktail[ingredient]}</p>
+                    }
+                })
+                }
                 
+                {/* <p>{this.props.cocktail.strMeasure1}</p>
+                <p>{this.props.cocktail.strMeasure2}</p>
+                <p>{this.props.cocktail.strMeasure3}</p>
+                <p>{this.props.cocktail.strMeasure4}</p>
+                <p>{this.props.cocktail.strMeasure5}</p>
+                <p>{this.props.cocktail.strMeasure6}</p>
+                <p>{this.props.cocktail.strMeasure7}</p>
+                <p>{this.props.cocktail.strMeasure8}</p>
+                <p>{this.props.cocktail.strMeasure9}</p>
+                <p>{this.props.cocktail.strMeasure10}</p>
+                <p>{this.props.cocktail.strMeasure11}</p>
+                <p>{this.props.cocktail.strMeasure12}</p>
+                <p>{this.props.cocktail.strMeasure13}</p>
+                <p>{this.props.cocktail.strMeasure14}</p>
+                <p>{this.props.cocktail.strMeasure15}</p> */}
             </div>
         )
     }
