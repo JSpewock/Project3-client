212b1f26560d686ece402bf947b0447fb4b0da63
Jeremy <jeremy.spewock@gmail.com>
2020-11-19 14:07:18 -0500

    fixed the form and added proper reset functionality (JS/BT)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,16 +4,15 @@ export default class Form extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: '',
-            instructions: '',
-            alcoholic: 'non-Alcoholic'
+            strDrink: '',
+            strInstructions: '',
+            strAlcoholic: 'non-Alcoholic'
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(event) {
-        console.log(event.target)
         this.setState({ [event.target.id] : event.target.value})
     }
 
@@ -22,22 +21,22 @@ export default class Form extends Component {
         fetch(this.props.baseURL, {
             method: 'POST',
             body: JSON.stringify({
-                name: this.state.name,
-                instructions: this.state.instructions,
-                alcoholic: this.state.alcoholic
+                strDrink: this.state.strDrink,
+                strInstructions: this.state.strInstructions,
+                strAlcoholic: this.state.strAlcoholic
             }),
             headers:{
                 'Content-Type': 'application/json'
-            } .then(res => res.json())
+            } 
+            }).then(res => res.json())
             .then(data => {
                 this.props.addCocktail(data)
                 this.setState({
-                    name: '',
-                    instructions: '',
-                    alcoholic: 'non-Alcoholic'
+                    strDrink: '',
+                    strInstructions: '',
+                    strAlcoholic: 'non-Alcoholic'
                 })
             })
-        })
     }
 
     render() {
@@ -45,26 +44,24 @@ export default class Form extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     {/* name */}
-                    <label htmlFor="name">
+                    <label htmlFor="strDrink">
                         Cocktail Name: 
-                        <input type="text" id="name" onChange={this.handleChange}/>
+                        <input type="text" id="strDrink" value={this.state.strDrink} onChange={this.handleChange}/>
                     </label>
                     {/* instructions */}
-                    <label htmlFor="instructions">
+                    <label htmlFor="strInstructions">
                         Intructions: 
-                        <input type="text" id="instructions" onChange={this.handleChange}/>
+                        <input type="text" id="strInstructions" value={this.state.strInstructions} onChange={this.handleChange}/>
                     </label>
                     {/* isAlcoholic */}
-                    <label htmlFor='Alcoholic'>
+                    <label htmlFor='strAlcoholic'>
                         Alcoholic
-                        <input type='radio' id='alcoholic' value='Alcoholic' name='Alcoholic' onChange={this.handleChange} />
+                        <input type='radio' id='strAlcoholic' value='Alcoholic' name='Alcoholic' onChange={this.handleChange} />
                     </label>
-                    <label htmlFor='Alcoholic'>
+                    <label htmlFor='strAlcoholic'>
                         non-Alcoholic
-                        <input type='radio' id='alcoholic' value='non-Alcoholic' name='Alcoholic' onChange={this.handleChange} />
+                        <input type='radio' id='strAlcoholic' value='non-Alcoholic' name='Alcoholic' onChange={this.handleChange} />
                     </label>
-
-                    
                     <input type="submit" value="Add Cocktail"/>
                 </form>
             </div>
