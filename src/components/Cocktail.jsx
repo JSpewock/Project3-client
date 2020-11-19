804526c16ca81346dba5a8a93a939cf15990be81
Jeremy <jeremy.spewock@gmail.com>
2020-11-19 14:30:14 -0500

    (previous commit was (JS/BT)) added images to the page (RL)

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -5,6 +5,7 @@ export default class Cocktail extends Component {
         return (
             <div id={this.props.cocktail.strDrink} key={this.props.cocktail._id}>
                 <h1>{this.props.cocktail.strDrink}</h1>
+                <img src={this.props.cocktail.strDrinkThumb} />
             </div>
         )
     }
